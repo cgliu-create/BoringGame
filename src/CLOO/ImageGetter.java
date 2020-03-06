6e7d54c23ce188247e023866a28fe949242d404a
cgliu-create <chrisliu2438@gmail.com>
2020-03-05 19:29:50 -0500

    starting new game

--- src/CLOO/ImageGetter.java
+++ src/CLOO/ImageGetter.java
@@ -9,11 +9,11 @@ import java.io.IOException;
 public class ImageGetter {
     //list all the images
     private BufferedImage error;
-    private BufferedImage snow0;
+    private BufferedImage steve;
     public ImageGetter(){
         //load in all the images
         try {// https://stackoverflow.com/questions/9864267/loading-image-resource/9866659#9866659
-            snow0 = ImageIO.read(getClass().getResource("/images/snow.png"));
+            steve = ImageIO.read(getClass().getResource("/images/steve.png"));
             error = ImageIO.read(getClass().getResource("/images/error.png"));
         } catch (IOException e) {
             e.printStackTrace();
@@ -22,7 +22,7 @@ public class ImageGetter {
     //method to retrieve images
     public BufferedImage getImage(int imgnum) {
         if (imgnum==0){
-            return snow0;
+            return steve;
         }
         return error;
     }
@@ -36,7 +36,7 @@ public class ImageGetter {
         double r = Math.toRadians(rot);
         double cx = W/2.0;
         double cy = H/2.0;
-        AffineTransform tx = AffineTransform.getRotateInstance(r, x+cx, y+cy);
+        AffineTransform tx = AffineTransform.getRotateInstance(r, cx, cy);
         AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
         Graphics2D g2d = (Graphics2D)window;
         g2d.drawImage(op.filter(img, null).getScaledInstance(W, H, Image.SCALE_SMOOTH), x, y, null);
