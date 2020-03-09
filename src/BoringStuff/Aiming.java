1a679c14de5e3c272c94f4ee9a0d9fd92164f0de
cgliu-create <chrisliu2438@gmail.com>
2020-03-08 22:45:32 -0400

    Bomb

--- src/BoringStuff/Aiming.java
+++ src/BoringStuff/Aiming.java
@@ -6,6 +6,8 @@ import java.awt.*;
 public class Aiming {
     private AliveObject thing;
     private int dx,dy;
+    private int cx, cy;
+    private int x, y;
     public Aiming(AliveObject thisThing){
         thing = thisThing;
     }
@@ -17,12 +19,20 @@ public class Aiming {
         int dir = thing.getDir();
         dy = (int)(10 * Math.sin(Math.toRadians(dir)));
     }
+    public void checkEnemyInSight(AliveObject otherThing){
+        //check if a line touches or intersects a circle
+        //distance from a point to a line...
+        cx = thing.getXPos()+thing.getWidth()/2;
+        cy = thing.getYPos()+thing.getHeight()/2;
+
+    }
     public void draw(Graphics window) {
-        int cx = thing.getXPos()+thing.getWidth()/2;
-        int cy = thing.getYPos()+thing.getHeight()/2;
-        setdx();setdy();
-        int x = cx + dx*20;
-        int y = cy + dy*20;
+        cx = thing.getXPos()+thing.getWidth()/2;
+        cy = thing.getYPos()+thing.getHeight()/2;
+        setdx();
+        setdy();
+        x = cx + dx*20;
+        y = cy + dy*20;
         window.setColor(Color.RED);
         window.drawLine(cx,cy,x,y);
     }
